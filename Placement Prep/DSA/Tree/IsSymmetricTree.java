34c7d311ff3e093384bcfde7c86e8966e37c4447
Prajjwal Yadav <94003570+prajjwal77@users.noreply.github.com>
2025-06-09 23:39:06 +0530

    Add files via upload

---
+++ Placement Prep/DSA/Tree/IsSymmetricTree.java
@@ -0,0 +1,19 @@
+public class IsSymmetricTree {
+     static class TreeNode {
+         int val;
+         TreeNode left;
+         TreeNode right;
+
+         TreeNode(int x) {
+             val = x;
+             left = null;
+             right = null;
+         }
+     }
+     public boolean check(TreeNode left, TreeNode right){
+          
+     }
+     public static boolean isSymmetric(TreeNode root) {
+        
+     }
+}
