dfccb3aed060e5a0303c5478ca321f8800cfb3cf
Prajjwal Yadav <94003570+prajjwal77@users.noreply.github.com>
2025-08-30 21:10:26 +0530

    Add files via upload

---
+++ Placement Prep/DSA/ArraysProblems/SearchingAndSorting/BinarySearch.java
@@ -0,0 +1,35 @@
+package ArrayRevision.SearchingAndSorting;
+
+import javax.xml.transform.Source;
+
+public class BinarySearch {
+	public static int binarySearch(int[] arr, int key) {
+		int low=0;
+		int high = arr.length;
+		
+		while(low<high) {
+			int mid = (low+high)/2;
+			if(arr[mid] == key) {
+				return mid;
+			}
+			else if(arr[mid]<key) {
+				low = mid+1;
+			}
+			else {
+				high = mid-1;
+			}
+		}
+		return -1;
+	}
+	public static void main(String[] args) {
+		int arr[] = {3,4,6,7,8,9};
+		int key = 7;
+		int idx = binarySearch(arr,key);
+		if(idx == -1) {
+			System.out.println("Not Found");
+		}
+		else {
+			System.out.println("Element Found Ar Index : "+idx);
+		}
+	}
+}
