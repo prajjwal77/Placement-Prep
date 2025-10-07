136df6f06b51210cf1b7f1c7748ba168c75ee3ef
Prajjwal Yadav <94003570+prajjwal77@users.noreply.github.com>
2025-10-07 12:32:47 +0530

    Add files via upload

---
+++ Placement Prep/DSA/String/AnagramString.java
@@ -0,0 +1,23 @@
+package String;
+
+import java.util.Arrays;
+
+public class AnagramString {
+	public static boolean checkAnagram(String str1, String str2) {
+		if(str1.length() != str2.length()){
+			return false;
+		}
+		
+		char[] arr1 = str1.toCharArray();
+		char[] arr2 = str2.toCharArray();
+		
+		Arrays.sort(arr1);
+		Arrays.sort(arr2);
+		
+		return Arrays.equals(arr1, arr2);
+	}
+	
+	public static void main(String[] args) {
+		System.out.println("Anagram string : "+checkAnagram("listen", "silednt"));
+	}
+}
